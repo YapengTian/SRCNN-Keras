943b6bf0a2e9c144d8bee158ab86ec8c09805693
YapengTian <typ14@mails.tsinghua.edu.cn>
2017-03-04 16:20:46 +0800

    Revert "test"
    This reverts commit e5821a0604c66220e7bf0a651061f78f4faffede.

--- test/utils/savefile.m
+++
@@ -1,9 +0,0 @@
-function [] = savefile(imLR, ori_HR, im_rgb,imbicubic, filename)
-    
-    filename = filename(1:end-4);
-%     imLR = imresize( ori_HR, 1.0/3, 'Bicubic');
-    imwrite(uint8(imLR),  ['Result\', filename, '_LR.bmp']);
-    imwrite(uint8(ori_HR),  ['Result\', filename, '_HR.bmp']);
-    imwrite(uint8(im_rgb),  ['Result\', filename, '_Vdsr_noise.bmp']);
-    imwrite(uint8(imbicubic),  ['Result\', filename, '_bicubic.bmp']);
-end
